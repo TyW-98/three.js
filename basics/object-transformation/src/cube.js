8af82183668c10336652c670801bb19031b7ef5f
wey1998 <yitwey1998@hotmail.com>
2023-04-25 17:18:08 +0100

    Object position and distance calculations

---
+++ basics/object-transformation/src/cube.js
@@ -0,0 +1,47 @@
+import * as THREE from 'three'
+
+const canvas = document.querySelector(".canvas");
+
+// Basic Red cube scene
+// Create a new scene
+const scene = new THREE.Scene();
+
+// Create new box object
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: "orange" });
+
+const cube = new THREE.Mesh(geometry, material);
+cube.position.set(-0.5, -0.75, -1)
+
+// Distance of cube from the center of the scene.
+const cubeDistanceFromCenter = cube.position.length()
+console.log(cubeDistanceFromCenter)
+
+// Normalise the length of the cube
+// cube.position.normalize()
+
+scene.add(cube);
+
+const sizes = {
+  width: 800,
+  height: 600
+};
+
+// Position Camera in scene
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.set(1,2,3)
+camera.lookAt(0,0,0)
+scene.add(camera);
+
+// Distance of cube from the camera
+const cubeDistanceFromCamera = cube.position.distanceTo(camera.position);
+console.log(cubeDistanceFromCamera)
+
+// Add renderer
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+
+renderer.setSize(sizes.width, sizes.height);
+
+renderer.render(scene,camera)
