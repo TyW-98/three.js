2bc5ec4ce061d704fea7569b106e46ec5ac1480e
wey1998 <yitwey1998@hotmail.com>
2023-04-25 18:09:04 +0100

    Add a group of cube objects to transform them together

--- basics/object-transformation/src/cube.js
+++ basics/object-transformation/src/cube.js
@@ -1,4 +1,4 @@
-import * as THREE from 'three'
+import * as THREE from "three";
 
 const canvas = document.querySelector(".canvas");
 
@@ -6,43 +6,69 @@ const canvas = document.querySelector(".canvas");
 // Create a new scene
 const scene = new THREE.Scene();
 
-// Create new box object
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: "orange" });
+// Grouping objects together
+const cubeGroup = new THREE.Group();
+scene.add(cubeGroup);
 
-const cube = new THREE.Mesh(geometry, material);
-cube.position.set(-0.5, -0.75, -1)
-cube.scale.set(2,1,1)
-cube.rotation.reorder("YXZ")
-cube.rotation.set(-0.25*Math.PI,1*Math.PI,0.1*Math.PI)
+const cube1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: "darkorange" })
+);
+cubeGroup.add(cube1);
+
+const cube2 = new THREE.Mesh(
+  new THREE.BoxGeometry(0.75, 0.75, 0.75),
+  new THREE.MeshBasicMaterial({ color: "skyblue" })
+);
+cube2.position.set(-0.875,0,0)
+cubeGroup.add(cube2);
+
+const cube3 = new THREE.Mesh(
+  new THREE.BoxGeometry(0.5, 0.5, 0.5),
+  new THREE.MeshBasicMaterial({ color: "green" })
+);
+cube3.position.set(-1.5,0,0)
+cubeGroup.add(cube3)
+
+cubeGroup.rotation.set(0,0.25*Math.PI,0)
+cubeGroup.position.set(0,0,-1)
+cubeGroup.scale.set(1,0.5,0.5)
+
+// Create new box object
+// const geometry = new THREE.BoxGeometry(1, 1, 1);
+// const material = new THREE.MeshBasicMaterial({ color: "orange" });
+
+// const cube = new THREE.Mesh(geometry, material);
+// cube.position.set(-0.5, -0.75, -1)
+// cube.scale.set(2,1,1)
+// cube.rotation.reorder("YXZ")
+// cube.rotation.set(-0.25*Math.PI,1*Math.PI,0.1*Math.PI)
 
 // Distance of cube from the center of the scene.
-const cubeDistanceFromCenter = cube.position.length()
-console.log(cubeDistanceFromCenter)
+const cubeDistanceFromCenter = cubeGroup.position.length();
+console.log(cubeDistanceFromCenter);
 
 // Normalise the length of the cube
 // cube.position.normalize()
 
 // Add Axes helper visualisation  (x = RED, y = GREEN, z = YELLOW)
-const axesHelper = new THREE.AxesHelper()
-scene.add(axesHelper)
-
-scene.add(cube);
+const axesHelper = new THREE.AxesHelper();
+scene.add(axesHelper);
 
 const sizes = {
   width: 800,
-  height: 600
+  height: 600,
 };
 
 // Position Camera in scene
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-camera.position.set(1,2,3)
-camera.lookAt(0,0,0)
+camera.position.set(1, 2, 3);
+camera.lookAt(cubeGroup.position);
 scene.add(camera);
 
 // Distance of cube from the camera
-const cubeDistanceFromCamera = cube.position.distanceTo(camera.position);
-console.log(cubeDistanceFromCamera)
+const cubeDistanceFromCamera = cubeGroup.position.distanceTo(camera.position);
+console.log(cubeDistanceFromCamera);
 
 // Add renderer
 const renderer = new THREE.WebGLRenderer({
@@ -51,4 +77,4 @@ const renderer = new THREE.WebGLRenderer({
 
 renderer.setSize(sizes.width, sizes.height);
 
-renderer.render(scene,camera)
+renderer.render(scene, camera);
