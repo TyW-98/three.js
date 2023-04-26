2355877e923384ee0521e3fe0750a06b5c38f65a
wey1998 <yitwey1998@hotmail.com>
2023-04-26 02:16:20 +0100

    Rotate the cube on X and Y axis using mouse position

--- basics/camera-basics/src/app.js
+++ basics/camera-basics/src/app.js
@@ -1,5 +1,22 @@
 import * as THREE from "three";
 
+const sizes = {
+  width: 800,
+  height: 600,
+};
+
+// Get mouse cursor position
+const cursorPosition = {
+  x: 0,
+  y: 0,
+  z: 0
+};
+window.addEventListener("mousemove", (event) => {
+  cursorPosition.x = event.clientX / sizes.width - 0.5;
+  cursorPosition.y = - (event.clientY / sizes.height - 0.5);
+  console.log(cursorPosition.x, cursorPosition.y);
+});
+
 const canvas = document.querySelector("canvas");
 
 const scene = new THREE.Scene();
@@ -10,20 +27,23 @@ const cube = new THREE.Mesh(
 );
 scene.add(cube);
 
-const sizes = {
-  width: 800,
-  height: 600,
-};
 const aspectRatio = sizes.width / sizes.height;
 // Perspective Camera attributes (FOV, aspect Ratio, closes object to show, furthest object to show).
-// const camera = new THREE.PerspectiveCamera(45 ,sizes.width / sizes.height, 0.1, 100);
-const camera = new THREE.OrthographicCamera(
-  -3 * aspectRatio,
-  3 * aspectRatio,
-  3,
-  -3
+const camera = new THREE.PerspectiveCamera(
+  45,
+  sizes.width / sizes.height,
+  0.1,
+  100
 );
-camera.position.set(2, 2, 3);
+// const camera = new THREE.OrthographicCamera(
+//   -3 * aspectRatio,
+//   3 * aspectRatio,
+//   3,
+//   -3,
+//   0.1,
+//   100
+// );
+camera.position.set(0,0,5);
 camera.lookAt(cube.position);
 scene.add(camera);
 
@@ -38,7 +58,11 @@ const clock = new THREE.Clock();
 const frames = () => {
   const elapsedTime = clock.getElapsedTime();
 
-  cube.rotation.y = elapsedTime;
+  //  cube.rotation.y = elapsedTime;
+
+  // Rotate cube on X and Y axis using mouse cursor position
+  camera.position.set(cursorPosition.x*Math.PI*2,cursorPosition.y*Math.PI*2,5)
+  camera.lookAt(cube.position)
 
   renderer.render(scene, camera);
 
